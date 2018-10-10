f31caf8945c907b73248780498c5b4d943b38964
Arjan Cupi <cupi.arjan@gmail.com>
2018-10-10 11:01:17 +0200

    refactor: readability

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-// https://s3-eu-west-1.amazonaws.com/dev-vitrines-files/dev-lambda-resize-image.zip
-// TODO update URL = https://dev-vitrines-files.s3-website-eu-west-1.amazonaws.com
-// http://dev-vitrines-files.s3-website-eu-west-1.amazonaws.com/800x600/C000064/1b8d23ad3aacf8752bd72192123573fb.jpg
-// https://7i8iyjew03.execute-api.eu-west-1.amazonaws.com/dev/v1?key=/800x600/C000064/1b8d23ad3aacf8752bd72192123573fb.jpg
-
+'use strict';
 const AWS = require('aws-sdk');
-const S3 = new AWS.S3({ signatureVersion: 'v4'});
+const S3 = new AWS.S3({signatureVersion: 'v4'});
 const Sharp = require('sharp');
 const BUCKET = process.env.BUCKET;
 const URL = process.env.URL;
 const ALLOWED_RESOLUTIONS = process.env.ALLOWED_RESOLUTIONS ? new Set(process.env.ALLOWED_RESOLUTIONS.split(/\s*,\s*/)) : new Set([]);
 
-exports.handler = function(event, context, callback) {
-  let newImgPath;
+exports.handler = function (event, context, callback) {
   const { queryStringParameters, Records } = event;
-  const { key } = queryStringParameters;
-  if (queryStringParameters && key && key !== "") {
-    console.info("Call from API Gateway.");
-    newImgPath = event.queryStringParameters.key;
-  } else if (Records && Records !== ""){
-    console.info("Call from S3 Bucket.");
-    const imgPath = Records[0].s3.object.key;
-    if(imgPath.startsWith("800x600")){
-      console.info("Call from S3 for the resize image");
+  let resizedImgPath;
+  // extract info from event
+  if (queryStringParameters && queryStringParameters.key && queryStringParameters.key !== "") {
+    console.info("call from API Gateway");
+    resizedImgPath = queryStringParameters.key;
+  } else if (Records && Records !== "") {
+    console.info("call from s3 bucket");
+    if (event.Records[0].s3.object.key.startsWith("800x600")) {
       return;
     }
-    newImgPath = `800x600/${imgPath}`;
+    let path = Records[0].s3.object.key;
+    let folder = path.substring(0,path.indexOf('/'));
+    let img = path.substring(path.length,path.indexOf('/'));
+    resizedImgPath = `${folder}/800x600/${img}`;
   } else {
-    console.error("Call error.");
+    console.error("can't extract info from payload, returning 403");
     callback(null, {
       statusCode: '403',
       headers: {},
@@ -34,41 +31,51 @@ exports.handler = function(event, context, callback) {
     });
     return;
   }
-  const match = newImgPath.match(/((\d+)x(\d+))\/(.*)/);
-  if(0 != ALLOWED_RESOLUTIONS.size && !ALLOWED_RESOLUTIONS.has(match[1])) {
+
+  // extract info from route params
+  const match = resizedImgPath.match(/((\d+)x(\d+))\/(.*)/);
+  const width = parseInt(match[2], 10);
+  const height = parseInt(match[3], 10);
+  const originalImgPath = match[4];
+
+  // prevent resizing for not supported resolutions
+  if (0 !== ALLOWED_RESOLUTIONS.size && !ALLOWED_RESOLUTIONS.has(match[1])) {
+    console.warn(`wanted resolution ${match[1]} is not allowed`);
     callback(null, {
       statusCode: '403',
       headers: {},
-      body: '',
+      body: `wanted resolution ${match[1]} is not allowed`,
     });
     return;
   }
-  const width = parseInt(match[2], 10);
-  const height = parseInt(match[3], 10);
-  const originalKey = match[4];
-  S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
+
+  S3.getObject({Bucket: BUCKET, Key: originalImgPath}).promise()
+    // get original img
     .then(data => Sharp(data.Body)
-    .withoutEnlargement()
-    .resize(width, height)
-    .min()
-    .toFormat('png')
-    .toBuffer()
-)
-.then(buffer => S3.putObject({
-    Body: buffer,
-    Bucket: BUCKET,
-    ContentType: 'image/png',
-    Key: newImgPath,
-  }).promise()
-)
-.then(() => callback(null, {
-    statusCode: '301',
-    headers: {'location': `${URL}/${newImgPath}`},
-    body: newImgPath,
-  })
-)
-.catch((err) => {
-    console.error(err);
-  callback(err);
-})
-}
+      .resize(width, height)
+      .min()
+      .withoutEnlargement()
+      .jpeg()
+      .toBuffer()
+    )
+    // upload resized image to s3
+    .then(buffer => S3.putObject({
+        Body: buffer,
+        Bucket: BUCKET,
+        ContentType: 'image/jpeg',
+        Key: resizedImgPath,
+      }).promise()
+    )
+    // return 301 with new resized img path
+    .then(() => callback(null, {
+        statusCode: '301',
+        headers: {'location': `${URL}/${resizedImgPath}`},
+        body: resizedImgPath,
+      })
+    )
+    // handle errors
+    .catch((err) => {
+      console.error(err);
+      callback(err);
+    })
+};
