f5a9038672b855f266d6edebfd5c9f52712e6dcf
Arjan Cupi <cupi.arjan@gmail.com>
2018-11-19 16:21:43 +0100

    test: moved s3 delete event file

--- index.js
+++ index.js
@@ -11,10 +11,16 @@ const getInfoFromPath = (path) => {
   const folder = match[2] === '' ? match[1] : `${match[1]}/${match[2]}`;
   const width = parseInt(match[3], 10);
   const height = parseInt(match[4], 10);
-  const file = match[5].replace('/','');
+  const file = match[5].replace('/', '');
   const originalImgPath = `${folder}/${file}`;
   const resolution = `${width}x${height}`;
-  console.log('getInfoFromPath' , {match, width, height, originalImgPath, resolution});
+  console.log('getInfoFromPath', {
+    match,
+    width,
+    height,
+    originalImgPath,
+    resolution
+  });
   return {match, width, height, originalImgPath, resolution, folder, file};
 };
 
@@ -32,88 +38,101 @@ exports.handler = function (event, context, callback) {
       headers: {},
       body: 'event :' + JSON.stringify(event, null, 2),
     });
+    return;
   } else if (isApiGatewayEvent) {
     resizedImgPath = queryStringParameters.key;
   } else if (isS3Event) {
-    const [ record ] = Records;
+    const [record] = Records;
     const {eventName} = record;
-    console.log('s3 event =>>>>', eventName);
     const isDeleteEvent = eventName === 'ObjectRemoved:Delete';
     const isPutEvent = eventName === 'ObjectCreated:Put';
     const path = Records[0].s3.object.key;
-    // do not resize non-img file formats
-    const ALLOWED_FORMATS = ['png','jpg','gif'];
-    ALLOWED_FORMATS.forEach((format) => {
+    console.log('path =>>>', path);
+    const ALLOWED_IMG_FORMATS = ['png', 'jpg', 'gif'];
+    ALLOWED_IMG_FORMATS.forEach((format) => {
+      // do not resize non-img file formats
       if (!path.includes(format)) callback();
     });
     const folder = path.substring(0, path.indexOf('/'));
     if (isPutEvent) {
-      console.log('call from S3 - event : put');
-      ALLOWED_RESOLUTIONS.forEach((resolution) => {
-        // prevent infinite loop : do not resize already resized imgs
-        if (path.includes(resolution)) return;
-        const [width, height] = resolution.split('x');
-        const img = path.substring(path.length, path.indexOf('/')).replace('/','');
-        resizedImgPath = `${folder}/${width}x${height}/${img}`;
-        console.log('resizing for each resolution : ', `${width}x${height}`, resizedImgPath);
-        resizeAndUploadToS3(path, parseInt(width,10), parseInt(height,10), resizedImgPath, callback);
-      });
-    } else if (isDeleteEvent) {
-      const match = path.match(/(\w+)\/?(\D*)\/(\d+)x(\d+)\/(\S+)/);
-      const hasResolution = match != null ;
-      console.log('prefix : ',hasResolution ? `${match[3]}x${match[4]}` : folder);
+      console.log('S3 isPutEvent');
       const params = {
         Bucket: BUCKET,
-        Prefix: hasResolution ? `${match[3]}x${match[4]}` : folder
+        Key: path
+      };
+      S3.headObject(params, (err) => {
+        if (err && err.code === 'NotFound') {
+          console.log("isPutEvent - NotFound");
+          // TODO first put, do nothing
+        } else {
+          console.log("isPutEvent - Found - update scenario : delete all" +
+            " resized folders for that img")
+          // TODO update scenario, delete all resized folders
+        }
+      });
+    } else if (isDeleteEvent) {
+      console.log('S3 isDeleteEvent');
+      const match = path.match(/(\w+)\/?(\D*)\/(\d+)x(\d+)\/(\S+)/);
+      const hasResolution = match != null;
+      if (hasResolution) return; // do nothing when deleting already resized imgs
+      const params = {
+        Bucket: BUCKET,
+        Prefix: `${folder}/`,
+        Delimiter: '/'
       };
       console.log('delete event : folder to delete =>>>', params);
       S3.listObjectsV2(params, (err, data) => {
-        console.log('delete event : data to delete =>>>', data.Contents.map(img => ({Key: img.Key})));
-        if (err) {
-          console.error(err, err.stack);
-        } else {
+        console.log('data =>>>', data);
+        console.log('data.CommonPrefixes =>>>', data.CommonPrefixes);
+        // list all resized img folders under C0XXXXX and delete them
+        data.CommonPrefixes.forEach(({Prefix}) => {
+          console.log('deleting =>>>> Prefix', Prefix);
           const params = {
             Bucket: BUCKET,
-            Delete: {
-              Objects: data.Contents.map(img => ({Key: img.Key}))
-            },
+            Prefix: Prefix,
           };
-          S3.deleteObjects(params, (err, data) => {
-            console.log('S3.deleteObjects params =>>>', JSON.stringify(params))
-            if (err) {
-              console.error(err, err.stack);
-            } else {
-              console.log('deleted following object', JSON.stringify(data));
-            }
+          console.log('delete event : folder to delete =>>>', params);
+          S3.listObjectsV2(params, (err, folder) => {
+            console.log('rezised folder data : ', folder);
+            const params = {
+              Bucket: BUCKET,
+              Delete: {
+                Objects: folder.Contents.map(img => ({Key: img.Key}))
+              },
+            };
+            S3.deleteObjects(params, (err, data) => {
+              if (err) {
+                console.error(err, err.stack);
+              } else {
+                console.log('deleted : ', data);
+              }
+            });
           });
-        }
-      });
-    } else {
-      // GET EVENT
-      const folder = path.substring(0, path.indexOf('/'));
-      const img = path.substring(path.length, path.indexOf('/'));
-      resizedImgPath = `${folder}/800x600/${img}`;
-      console.log("call from s3, resizedImgPath", resizedImgPath);
-    }
-  } else {
-    // extract info from path
-    const {match, width, height, originalImgPath, resolution} = getInfoFromPath(resizedImgPath);
-    const isResolutionAllowed = 0 !== ALLOWED_RESOLUTIONS.size && ALLOWED_RESOLUTIONS.has(resolution);
-    // prevent resizing for not supported resolutions
-    if (!isResolutionAllowed) {
-      console.warn(`wanted resolution ${match[1]} is not allowed`);
-      callback(null, {
-        statusCode: '403',
-        headers: {},
-        body: `wanted resolution ${match[1]} is not allowed`,
+        });
       });
       return;
+    } else {
+      console.log('S3 isGetEvent');
+      const folder = path.substring(0, path.indexOf('/'));
+      const img = path.substring(path.length, path.indexOf('/'));
+      const {resolution} = getInfoFromPath(path);
+      resizedImgPath = `${folder}/${resolution}/${img}`;
     }
-    const folder = path.substring(0, path.indexOf('/'));
-    const img = path.substring(path.length, path.indexOf('/'));
-    resizedImgPath = `${folder}/800x600/${img}`;
-    resizeAndUploadToS3(originalImgPath, width, height, resizedImgPath, callback);
   }
+  // extract info from path
+  const {match, width, height, originalImgPath, resolution} = getInfoFromPath(resizedImgPath);
+  const isResolutionAllowed = 0 !== ALLOWED_RESOLUTIONS.size && ALLOWED_RESOLUTIONS.has(resolution);
+  // prevent resizing for not supported resolutions
+  if (!isResolutionAllowed) {
+    console.warn(`wanted resolution ${match[1]} is not allowed`);
+    callback(null, {
+      statusCode: '403',
+      headers: {},
+      body: `wanted resolution ${match[1]} is not allowed`,
+    });
+    return;
+  }
+  resizeAndUploadToS3(originalImgPath, width, height, resizedImgPath, callback);
 };
 
 const resizeAndUploadToS3 = (originalImgPath, width, height, resizedImgPath, callback) => {
@@ -128,18 +147,18 @@ const resizeAndUploadToS3 = (originalImgPath, width, height, resizedImgPath, cal
       .toBuffer()
     )
     .then(buffer => {
-     // get img format from buffer in order to set correct ContentType
-     Sharp(buffer)
-        .metadata()
-        .then(({format}) => {
-          // upload resized image to s3 bucket
-          S3.putObject({
-            Body: buffer,
-            Bucket: BUCKET,
-            ContentType: `image/${format}`,
-            Key: resizedImgPath,
-          }).promise()
-        });
+        // get img format from buffer in order to set correct ContentType
+        Sharp(buffer)
+          .metadata()
+          .then(({format}) => {
+            // upload resized image to s3 bucket
+            S3.putObject({
+              Body: buffer,
+              Bucket: BUCKET,
+              ContentType: `image/${format}`,
+              Key: resizedImgPath,
+            }).promise()
+          });
       }
     )
     // return 301 with resized img path
